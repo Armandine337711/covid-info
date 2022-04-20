55d0595118ca8dfb1b3053a512aff6b4c2330497
Armandine BARGE <armandine.barge@ntymail.com>
2022-04-20 12:26:40 +0200

    beginig heroku configuration

--- c19-client/components/InfoMaladie.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const InfoMaladie = () => {
-  return <div>InfoMaladie</div>;
-};
-
-export default InfoMaladie;
--- c19-client/components/common/Separator.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Separator = () => {
-  return <div className="w-100 bg-primary">&nbsp;</div>;
-};
-
-export default Separator;
